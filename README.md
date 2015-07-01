4057e6c1c961fee097771ac44449c2170009a3ea
kaboomfox <louise.s.fox@gmail.com>
2015-07-01 06:04:39 +0000

    PyV8 for linix x64

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# pyv8-linux-64x
-wrapper around pyv8-linux-64x binary to allow pip install
+PyV8-linux-x64
+==============
+
+This is a wrapper around pyv8-linux-64x binary to allow pip install.
+The origian source of the binary came from (https://github.com/emmetio/pyv8-binaries).
+
+Installation
+------------
+To install with pip directly from this git repo, use the command:
+```
+pip install -e git://github.com/kaboomfox/PyV8-linux-x64#egg=pyv8
+```
+
+You can then ``from pyv8 import PyV8`` in python and proceed with the examples shown on the pyv8 website.
---
+++ setup.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="PyV8",
+    version="1.0",
+    author="The internet",
+    author_email="louise.s.fox@gmail.com",
+    description="wrapper for PyV8 binary for Linux x64",
+    long_description=read("README.md"),
+    license="",
+    url="https://github.com/louisef/pyv8-linux-x64",
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={
+        'pyv8': ['*.so']
+    },
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: Linux",
+        "Programming Language :: Python",
+    ]
+)
